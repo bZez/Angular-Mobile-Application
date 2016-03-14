f5df01f7f5e36f7926610679d53af97f0204178d
Erich L <erichleonhardt1991@outlook.com>
2016-03-14 19:38:29 +0100

    Result

---
+++ js/services.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Erich Leonhardt on 10.07.2014
+	url: https://www.odesk.com/users/~0107a1f34f94283892
+ */
+
+angular.module("DPFMA.services", [])
+	.constant("servicesConfig", {
+		urlRoot: "./json/",
+		urlSub: {
+			labels: "Labels.json",
+			patients: "Patients.json",
+			settings: "Settings.json",
+			analyses: "CodeRep.json",
+			clinical: "ClinicalCodes.json",
+		}
+    })
+    .factory("$apiConnector", ["servicesConfig", "$http", "$global", function(servicesConfig, $http, $global) {
+        return {
+			getData: function($scope, type, func, params, callback){
+				var ajaxURL = servicesConfig.urlRoot + servicesConfig.urlSub[func];
+				
+				////
+				$scope.$parent.loadingBar = true;
+				$http({method: type, url: ajaxURL, headers: {"Content-Type": "application/json; charset=UTF-8"}, data: params})
+					.success(function(response, status, headers, servicesConfig){
+						$scope.$parent.loadingBar = false;
+
+						if(callback){
+							callback(response);
+						}
+					})
+					.error(function(response, status, headers, servicesConfig){
+						$scope.$parent.loadingBar = false;
+						alert("Ajax Error");
+						
+						if(callback){
+							callback(response);
+						}
+					});
+				return;
+            },
+        };
+    }]);
